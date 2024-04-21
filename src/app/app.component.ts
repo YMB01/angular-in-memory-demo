cfdca630b021dd4cb3f67a48a72b75d87e2e7cab
YMB01 <25132dev@gmail.com>
2024-04-21 12:13:54 +0300

    angular in memory demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-in-memory-demo';
+export class AppComponent implements OnInit {
+  items: any[] = [];
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.getItems();
+  }
+
+  getItems(): void {
+    this.dataService.getItems()
+      .subscribe(items => this.items = items);
+  }
 }
